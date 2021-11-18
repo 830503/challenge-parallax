00dd7c251877cd30d2cc7d32eb41d688d605685a
830503 <lixiaoqi19830503@gmail.com>
2021-11-18 15:43:26 +0100

    jump function works

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
-let char = document.querySelector('.player');
-let obstacle = document.querySelector('.marsh');
+document.addEventListener('DOMContentLoaded', () => {
+    let char = document.querySelector('.player');
+    let obstacle = document.querySelector('.marsh');
 
-function jump() {
-    if (char.classList !== 'jump') {
-        char.classList.add('jump');
-        setTimeout(function () {
-            char.classList.remove('jump');
-        }, 300)
+    function control(e) {
+        if (e.keyCode === 32) {
+            jump();
+        }
     }
-}
 
-let CheckAlive = setInterval(() => {
-    let charTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'));
-    // console.log(charTop);
-    let obsLeft = parseInt(window.getComputedStyle(marsh).getPropertyValue('left'));
-    //console.log(obsLeft);
-    if (obsLeft < 100 && obsLeft > 0 && charTop >= 500){
-        //console.log('collision');
+    function jump() {
+        if (char.classList !== 'jump') {
+            char.classList.add('jump');
+            setTimeout(function () {
+                char.classList.remove('jump');
+            }, 500)
+        }
     }
-}, 10);
+
+    let CheckAlive = setInterval(() => {
+        let charTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'));
+        // console.log(charTop);
+        let obsLeft = parseInt(window.getComputedStyle(marsh).getPropertyValue('left'));
+        //console.log(obsLeft);
+        if (obsLeft < 40 && obsLeft > 0 && charTop >= 500){
+            //console.log('collision');
+            alert("Game over");
+        }
+    }, 10);
+    
+    
+    document.addEventListener('keydown', control)
 
 
-document.addEventListener('keydown', function (event) {
-    // console.log(event.key);
-    jump();
-});
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
