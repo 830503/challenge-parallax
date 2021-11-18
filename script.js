b49f9101c7ca14f4703f13c01bb178d14fbdaa14
830503 <lixiaoqi19830503@gmail.com>
2021-11-18 12:29:38 +0100

    added collision, work not well

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-let parallax = document.querySelectorAll('.parallax');
 let char = document.querySelector('.player');
+let obstacle = document.querySelector('.marsh');
 
+function jump() {
+    if (char.classList !== 'jump') {
+        char.classList.add('jump');
+        setTimeout(function () {
+            char.classList.remove('jump');
+        }, 300)
+    }
+}
+
+let CheckAlive = setInterval(() => {
+    let charTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'));
+    // console.log(charTop);
+    let obsLeft = parseInt(window.getComputedStyle(marsh).getPropertyValue('left'));
+    //console.log(obsLeft);
+    if (obsLeft < 100 && obsLeft > 0 && charTop >= 500){
+        //console.log('collision');
+    }
+}, 10);
 
 
 document.addEventListener('keydown', function (event) {
     // console.log(event.key);
     jump();
-
-
-
 });
-
-function jump() {
-    if (player.classList !== 'jump') {
-        player.classList.add('jump');
-        setTimeout(function () {
-            char.classList.remove('jump');
-        }, 300)
-    }
-
-}
