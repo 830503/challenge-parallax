255d40122ec7a124a19a728b4a56f95e042430e6
830503 <lixiaoqi19830503@gmail.com>
2021-11-18 11:06:54 +0100

    added one obstacle

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 let parallax = document.querySelectorAll('.parallax');
+let char = document.querySelector('.player');
+
+
+
+document.addEventListener('keydown', function (event) {
+    // console.log(event.key);
+    jump();
+
+
+
+});
+
+function jump() {
+    if (player.classList !== 'jump') {
+        player.classList.add('jump');
+        setTimeout(function () {
+            char.classList.remove('jump');
+        }, 300)
+    }
+
+}
