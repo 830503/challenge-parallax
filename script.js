ddc4f8e92fd90a9dcd2604cf36e2740a485014a3
830503 <lixiaoqi19830503@gmail.com>
2021-11-19 11:44:58 +0100

    added audio, but does not work

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     let char = document.querySelector('.player');
     let obstacle = document.querySelector('.marsh');
+
+    let music = new Audio('music/music.mov');
+    let gameover = new Audio('music/gameover.mp3');
+    let jumpMusic = new Audio('jump.mp3');
+
     let score = document.getElementById('score');
     let counter = 0;
+
+    music.loop = true;
+    music.play();
+    // music.volume = 0.5;
 
     function control(e) {
         if (e.keyCode === 32) {
@@ -13,12 +22,14 @@ document.addEventListener('DOMContentLoaded', () => {
     function jump() {
         if (char.classList !== 'jump') {
             char.classList.add('jump');
+            jumpMusic.play();
             setTimeout(function () {
                 char.classList.remove('jump');
+                jumpMusic.paused();
+                jumpMusic.currentTime = 0;
             }, 500)
         }
     }
-
 
     let CheckAlive = setInterval(() => {
         let charTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'));
@@ -27,8 +38,10 @@ document.addEventListener('DOMContentLoaded', () => {
         //console.log(obsLeft);
         
         if (obsLeft < 40 && obsLeft > 0 && charTop >= 500){
-            
             alert("Game over.");
+            music.paused();
+            gameover.play();
+            
             counter=0;
 
             //console.log('collision');
@@ -36,21 +49,9 @@ document.addEventListener('DOMContentLoaded', () => {
             counter++;
             score.innerHTML = Math.floor(counter/100);
         }
-
-        
     }, 10);
     
     document.addEventListener('keydown', control);
-
-
-
-
-
-
-
-
-
-
 
 })
 
